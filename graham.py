9de812275d5d26117f6400ae96cc44582a52647c
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2006-12-02 22:36:17 +0000

    - fixed stupid bug - removed SVG output
    git-svn-id: file:///mnt/data/SVNrepo@347 a13746d7-5020-4d46-8679-245a07f888a7

--- graham.py
+++ graham.py
@@ -1,4 +1,4 @@
-def graham(points):
+def convex_hull(points):
 	n = len(points)
 	if n < 3:
 		return points
@@ -7,67 +7,52 @@ def graham(points):
 	cx  = sum(x for x,y in points)/float(n)
 	cy  = sum(y for x,y in points)/float(n)
 
-	drawing.circle((cx,cy), 5)
-
-	# move points by vecotr -cx,-cy
+	# move points by vector (-cx,-cy)
 	tmp = [(x-cx, y-cy, i) for i, (x, y) in enumerate(points)]
 
 	# sort them lexicographically
-	def alpha( (x, y, i) ):
+	def alpha(x, y):
 		d = float(abs(x) + abs(y))
-		if x >= 0 and y >= 0:	return y/d
-		if x <= 0 and y >= 0:	return 2.0 - y/d
-		if x <= 0 and y <= 0:	return 2.0 - y/d
-		if x >= 0 and y <= 0:	return 4.0 + y/d
+		if x >= 0 and y >= 0: return y/d
+		if x <= 0 and y >= 0: return 2.0 - y/d
+		if x <= 0 and y <= 0: return 2.0 + abs(y)/d
+		if x >= 0 and y <= 0: return 4.0 - abs(y)/d
 	
-	tmp.sort(key=alpha)
+	def sort_fun( (x1, y1, i1), (x2, y2, i2) ):
+		a1 = alpha(x1, y1)
+		a2 = alpha(x2, y2)
+		if a1 == a2:
+			r1 = x1*x1 + y1*y1
+			r2 = x2*x2 + y2*y2
+			return cmp(r1, r2)
+		else:
+			return cmp(a1, a2)
+		
+	tmp.sort(sort_fun)
 
-	# get a on convex hull - point that has min x
+	# get a point on convex hull - point that has min x
 	imin = 0
-	for i, (x, _, _) in enumerate(tmp):
-		if x < tmp[imin]:
+	for i, (x, y, _) in enumerate(tmp):
+		if y < tmp[imin][1]:
 			imin = i
-	
+		elif y == tmp[imin][1]:
+			if x < tmp[imin][0]:
+				imin = i
+
 	tmp = tmp[imin:] + tmp[:imin]
 
-
 	# find convex hull
-
-	file = 0
-
 	ai = 0
 	while ai < n:
-		drawing.clear_drawing()
-		for x,y in points:
-			drawing.circle((x,y), 4)
-		if n == 3:
-			break
-
 		bi = (ai + 1) % n
-		ci = (bi + 1) % n
+		ci = (ai + 2) % n
 
 		a = tmp[ai]
 		b = tmp[bi]
 		c = tmp[ci]
-		det = -(c[1]-a[1]) * (b[0]-a[0]) + (c[0]-a[0]) * (b[1]-a[1])
+		det = (c[0]-a[0]) * (b[1]-a[1]) - (c[1]-a[1]) * (b[0]-a[0])
 
-		
-		pp = [(x+cx, y+cy) for x,y,i in tmp[:ai+1]]
-		if pp:
-			drawing.polyline(pp, style='tested')
-		drawing.line((cx+a[0],cy+a[1]), (cx+c[0],cy+c[1]))
-
-		if det > 0:
-			drawing.line((cx+a[0],cy+a[1]), (cx+b[0],cy+b[1]), style='rejected')
-			drawing.circle((cx+b[0],cy+b[1]), 3, style='rejected')
-		else:
-			drawing.line((cx+a[0],cy+a[1]), (cx+b[0],cy+b[1]), style='accepted')
-			drawing.circle((cx+b[0],cy+b[1]), 3, style='accepted')
-
-		drawing.save('tmp%02d.svg' % file, prettyXML=True)
-		file += 1
-			
-		if det > 0:
+		if det >= 0.0:
 			tmp.pop(bi)
 			n = n - 1
 			if ai > 0:
@@ -76,42 +61,4 @@ def graham(points):
 			ai = ai + 1
 
 
-	return [i for (x,y,i) in tmp]
-
-if __name__ == '__main__':
-	import random
-
-	from random import randint
-	from SVG    import SVGDrawing
-
-	class Dummy: pass
-	options = Dummy()
-
-	margin = 5
-	
-	options.n = 8
-	options.s = 300
-
-	drawing = SVGDrawing(options.s + 2*margin, options.s + 2*margin)
-	drawing.dx = margin
-	drawing.dy = margin
-	drawing.new_layer('default')
-	drawing.new_style('!circle',	('fill', 'black'))
-	drawing.new_style('tested', 	('stroke', '#aaa'), ('fill', 'none'))
-	drawing.new_style('rejected',	('stroke', 'red'), ('fill', 'red'), ('stroke-width', '2'))
-	drawing.new_style('accepted',	('stroke', 'blue'), ('fill', 'blue'))
-	drawing.new_style('default',	('stroke', 'black'))
-
-	def point():
-		return (random.randint(0, options.s),
-		        random.randint(0, options.s))
-
-	random.seed(20)
-	points  = [point() for i in xrange(options.n)]
-	for x,y in points:
-		drawing.circle((x,y), 4)
-
-	indexes = graham(points)
-	#drawing.polygon([points[i] for i in indexes])
-
-	#drawing.save('graham.svg', prettyXML=True)
+	return [(x+cx, y+cy, i) for x, y, i in tmp]
