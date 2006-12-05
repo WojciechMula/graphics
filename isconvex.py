0d8236533de2b2695a9794aecea478caca078607
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2006-12-05 20:36:09 +0000

    added vertex_order
    git-svn-id: file:///mnt/data/SVNrepo@365 a13746d7-5020-4d46-8679-245a07f888a7

--- isconvex.py
+++ isconvex.py
@@ -42,6 +42,37 @@ def isconvex(poly):
 				return False             # polygon isn't convex
 	return True
 
+def vertex_order(poly):
+	n = len(poly)
+	if n < 3:
+		# not a polygon
+		return 'unspecified'
+
+
+	valid_side = None
+	for i in xrange(n):
+		j = (i+1) % n
+		k = (i+2) % n
+
+		# Get three adjecent vertices
+		x0, y0 = poly[i] # A
+		x1, y1 = poly[j] # B
+		x2, y2 = poly[k] # C 
+
+		# Get side  (line is given as a*x+b*y+c=0, where c=0)
+		side = (y1-y0)*(x2-x0) - (x1-x0)*(y2-y0)
+
+		if side != 0.0:
+			if valid_side == None:
+				valid_side = side > 0.0
+			elif (side > 0.0) != valid_side: # other side:
+				return 'unspecified'     # polygon isn't convex
+	
+	if valid_side > 0:
+		return 'CCW'
+	else:
+		return 'CW'
+
 if __name__ == '__main__':
 	a = 0.0
 	b = 1.0
