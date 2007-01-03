e6f52133f974ab535b631a8ff2086b1e8d46ac01
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-01-03 21:29:55 +0000

    use function 'vertex_order' to implement 'isconvex'
    git-svn-id: file:///mnt/data/SVNrepo@383 a13746d7-5020-4d46-8679-245a07f888a7

--- isconvex.py
+++ isconvex.py
@@ -9,9 +9,19 @@ def isconvex(poly):
 	if n < 3:
 		# not a polygon
 		return False
-	if n == 3:
+	elif n == 3:
 		# triangles are convex polygons
 		return True
+	else:
+		return vertex_order(poly) in ['CW', 'CCW']
+
+
+def vertex_order(poly):
+	n = len(poly)
+	if n < 3:
+		# not a polygon
+		return 'unspecified'
+
 
 	# Algorithm:
 	#   We check all triples of adjecent points. For example if polygon
@@ -36,37 +46,10 @@ def isconvex(poly):
 		side = (y1-y0)*(x2-x0) - (x1-x0)*(y2-y0)
 
 		if side != 0.0:
-			if valid_side == None:
+			if valid_side is None:
 				valid_side = side > 0.0
 			elif (side > 0.0) != valid_side: # other side:
-				return False             # polygon isn't convex
-	return True
-
-def vertex_order(poly):
-	n = len(poly)
-	if n < 3:
-		# not a polygon
-		return 'unspecified'
-
-
-	valid_side = None
-	for i in xrange(n):
-		j = (i+1) % n
-		k = (i+2) % n
-
-		# Get three adjecent vertices
-		x0, y0 = poly[i] # A
-		x1, y1 = poly[j] # B
-		x2, y2 = poly[k] # C 
-
-		# Get side  (line is given as a*x+b*y+c=0, where c=0)
-		side = (y1-y0)*(x2-x0) - (x1-x0)*(y2-y0)
-
-		if side != 0.0:
-			if valid_side == None:
-				valid_side = side > 0.0
-			elif (side > 0.0) != valid_side: # other side:
-				return 'unspecified'     # polygon isn't convex
+				return 'nonconvex'           # polygon isn't convex
 	
 	if valid_side > 0:
 		return 'CCW'
@@ -83,3 +66,5 @@ if __name__ == '__main__':
 
 	print isconvex(p1)
 	print isconvex(p2)
+
+# vim: ts=4 sw=4
