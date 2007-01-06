d7fc04f9fb814afaf9f8d4593ef7c715768af3c9
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-01-06 15:42:30 +0000

    revert
    git-svn-id: file:///mnt/data/SVNrepo@409 a13746d7-5020-4d46-8679-245a07f888a7

--- graham.py
+++ graham.py
@@ -1,38 +1,32 @@
 def convex_hull(points):
 	n = len(points)
 	if n < 3:
-		return points
+		return None
+	elif n == 3:
+		return [(p, i) for i, p in enumerate(points)]
 
 	# calcuate centroid
 	cx  = sum(x for x,y in points)/float(n)
 	cy  = sum(y for x,y in points)/float(n)
 
+	def alpha(x, y):
+		r = x*x + y*y
+		d = float(abs(x) + abs(y))
+		if x >= 0 and y >= 0: return (y/d, r)
+		if x <= 0 and y >= 0: return (2.0 - y/d, r)
+		if x <= 0 and y <= 0: return (2.0 + abs(y)/d, r)
+		if x >= 0 and y <= 0: return (4.0 - abs(y)/d, r)
+
 	# move points by vector (-cx,-cy)
-	tmp = [(x-cx, y-cy, i) for i, (x, y) in enumerate(points)]
+	tmp = [ (x, y, i, alpha(x-cx, y-cy))
+		for i, (x, y) in enumerate(points)]
 
 	# sort them lexicographically
-	def alpha(x, y):
-		d = float(abs(x) + abs(y))
-		if x >= 0 and y >= 0: return y/d
-		if x <= 0 and y >= 0: return 2.0 - y/d
-		if x <= 0 and y <= 0: return 2.0 + abs(y)/d
-		if x >= 0 and y <= 0: return 4.0 - abs(y)/d
-	
-	def sort_fun( (x1, y1, i1), (x2, y2, i2) ):
-		a1 = alpha(x1, y1)
-		a2 = alpha(x2, y2)
-		if a1 == a2:
-			r1 = x1*x1 + y1*y1
-			r2 = x2*x2 + y2*y2
-			return cmp(r1, r2)
-		else:
-			return cmp(a1, a2)
-		
-	tmp.sort(sort_fun)
+	tmp.sort(key=lambda (x, y, i, v): v)
 
-	# get a point on convex hull - point that has min x
+	# get a point on convex hull - point that has min. y and x
 	imin = 0
-	for i, (x, y, _) in enumerate(tmp):
+	for i, (x, y, _, _) in enumerate(tmp):
 		if y < tmp[imin][1]:
 			imin = i
 		elif y == tmp[imin][1]:
@@ -44,16 +38,12 @@ def convex_hull(points):
 	# find convex hull
 	ai = 0
 	while ai < n:
-		bi = (ai + 1) % n
-		ci = (ai + 2) % n
+		xa, ya, _, _ = tmp[ai]
+		xb, yb, _, _ = tmp[(ai + 1) % n]
+		xc, yc, _, _ = tmp[(ai + 2) % n]
 
-		a = tmp[ai]
-		b = tmp[bi]
-		c = tmp[ci]
-		det = (c[0]-a[0]) * (b[1]-a[1]) - (c[1]-a[1]) * (b[0]-a[0])
-
-		if det >= 0.0:
-			tmp.pop(bi)
+		if (xc-xa) * (yb-ya) - (yc-ya) * (xb-xa) >= 0.0:
+			tmp.pop((ai + 1) % n)
 			n = n - 1
 			if ai > 0:
 				ai = ai - 1
@@ -61,4 +51,6 @@ def convex_hull(points):
 			ai = ai + 1
 
 
-	return [(x+cx, y+cy, i) for x, y, i in tmp]
+	return [((x, y), i) for (x, y, i, _) in tmp]
+
+# vim: ts=4 sw=4
