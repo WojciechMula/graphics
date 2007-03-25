e677fbaf94294d4c7f5298623b804c6ee5c0acf8
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-03-25 16:27:30 +0000

    removed samples
    git-svn-id: file:///mnt/data/SVNrepo@854 a13746d7-5020-4d46-8679-245a07f888a7

--- elarc_aabb.py
+++ elarc_aabb.py
@@ -1,13 +1,12 @@
-from math import sin, cos, sqrt, pi, atan, atan2
-import sys
+# wm, $Date: 2007-03-25 16:27:30 $, $Revision: 1.3 $
 
-def ellipse_aabb(a, b, rot=0.0):
-	dx = sin(rot)
-	dy = cos(rot)
-	return atan2(dy*b/a, dx), atan2(-dx*b/a, dy)
+from math import sin, cos, pi, atan2
+
+def ellipse_tp(a, b, dx, dy):
+	return atan2(dy*b/a, dx)
 
 
-def arc_bbox(x0, y0, rx, ry, start=0.0, end=2*pi, rot=0.0):
+def elliptical_arc_bbox(x0, y0, rx, ry, start=0.0, end=2*pi, rot=0.0):
 	cr = cos(rot)
 	sr = sin(rot)
 
@@ -16,82 +15,32 @@ def arc_bbox(x0, y0, rx, ry, start=0.0, end=2*pi, rot=0.0):
 		y = sin(a)*ry
 		return (x*cr - y*sr, x*sr + y*cr)
 	
-	a1, a2 = ellipse_aabb(rx, ry, rot)
-	angles = [a1, a2, a1+pi, a2+pi]
-	
+	def angle(a):
+		if a < 0.0:
+			return a + 2*pi
+		elif a > 2*pi:
+			return a - 2*pi 
+		else:
+			return a
+
 	X = [P(start)[0], P(end)[0]]
 	Y = [P(start)[1], P(end)[1]]
-	
-	if end > 2*pi:
-		start -= 2*pi
-		end   -= 2*pi
 
-	for a in angles:
-		if a > end:
-			a -= 2*pi
-		
-		x, y = P(a)
-		color = "red"
-		if start <= a <= end:
-			X.append(x)
-			Y.append(y)
-			color = "blue"
-		print '<circle cx="%f" cy="%f" r="2" fill="%s" stroke="%s"/>' % (x0+x, y0+y, color, color)
+	start = angle(start)
+	end   = angle(end)
+	a1    = ellipse_tp(rx, ry, sr, cr)
+	a2    = ellipse_tp(rx, ry, -cr, sr)
 
-	return (min(X), min(Y)), (max(X), max(Y))
+	for a in map(angle, [a1, a2, a1+pi, a2+pi]):
+		if start < end:
+			if start <= a <= end:
+				x, y = P(a)
+				X.append(x)
+				Y.append(y)
+		else:
+			if not (end < a < start):
+				x, y = P(a)
+				X.append(x)
+				Y.append(y)
 
-head = """<?xml version="1.0" ?>
-<!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
-<svg height="400" width="400" xmlns="http://www.w3.org/2000/svg">
-"""
-
-tail = "</svg>"
-
-def arc_points(x0, y0, rx, ry, start, end, rot):
-	cr = cos(rot)
-	sr = sin(rot)
-
-	def P(a):
-		x = cos(a)*rx
-		y = sin(a)*ry
-		return (x*cr - y*sr, x*sr + y*cr)
-	
-	n  = 360/8
-	da = (end-start)/(n-1)
-	d  = []
-	for i in xrange(n):
-		a    = start + i*da
-		x, y = P(a)
-		d.append(x+x0)
-		d.append(y+y0)
-	return ' '.join(map(str, d))
-
-from random import random
-
-print head
-for i in xrange(10):
-	x = random()*400
-	y = random()*400
-#	x = 200
-#	y = 200
-	rx = (0.1+random())*80
-	ry = (0.1+random())*80
-	# XXX
-	#ry = rx
-	start = random()*2*pi
-	end   = start + random()*2*pi
-	rot   = random()*2*pi
-
-	(xmin, ymin), (xmax, ymax) = arc_bbox(x, y, rx, ry, start, end, rot)
-	xmin += x
-	xmax += x
-	ymin += y
-	ymax += y
-
-	print '<rect x="%0.2f" y="%0.2f" width="%0.2f" height="%0.2f" fill="none" stroke="red" stroke-width="0.5"/>' % (xmin, ymin, xmax-xmin, ymax-ymin)
-
-	print '<polyline points="%s" fill="none" stroke="#aaa"/>' % arc_points(x, y, rx, ry, 0, 2*pi, rot)
-	print '<polyline points="%s" fill="none" stroke="#000"/>' % arc_points(x, y, rx, ry, start, end, rot)
-print tail
-
-# vim: ts=4 sw=4 nowrap
+	return (x0 + min(X), y0 + min(Y)), (x0 + max(X), y0 + max(Y))
